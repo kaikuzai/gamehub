434ea5cbf3eb8c94a56a07bf2c4d9cb9ce189bdf
Dylan <dylan.okyere@gmail.com>
2023-11-30 22:00:50 +0100

    custom hook to fetch games

---
+++ gamehub/src/hooks/useGames.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { AxiosError, CanceledError } from "axios";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const controller = new AbortController();
+
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState<AxiosError>();
+
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
