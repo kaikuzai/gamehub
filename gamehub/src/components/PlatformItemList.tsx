dbbcd41d53a1e81369e017b03e2ce165e29e7558
Dylan <dylan.okyere@gmail.com>
2023-12-10 15:59:06 +0100

    added game icons

---
+++ gamehub/src/components/PlatformItemList.tsx
@@ -0,0 +1,42 @@
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaApple,
+  FaXbox,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe } from "react-icons/bs";
+import { SiNintendo } from "react-icons/si";
+import { IconType } from "react-icons/lib";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformItemList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    nintendo: SiNintendo,
+    xbox: FaXbox,
+    linux: FaLinux,
+    android: FaAndroid,
+    playstation: FaPlaystation,
+    mac: FaApple,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginTop={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500"></Icon>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformItemList;
--- gamehub/src/services/api-client.ts
+++ gamehub/src/services/api-client.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default axios.create({
     baseURL: 'https://api.rawg.io/api', 
-    params: {key: '9455e1e98232420587e223a8b3065377'}, 
+    params: {key: '3e15ce07218a48659d88959ccf52c562'}, 
 
 })
 
